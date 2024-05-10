7d4fd03a190ef236f8e3c2b48a516f972756d1de
Diego Brunari <diego.skp@hotmail.com>
2024-05-10 01:52:27 -0300

    feat: readme to instructions how project work

---
+++ README.md
@@ -0,0 +1,12 @@
+PHP + PDO + MYSQL
+
+- Necessário ambiente no Xampp;
+
+- No Xampp ativar os serviços de Apache e MySQL;
+
+- Acessar localhost/phpmyadmin e criar um banco de dados com o nome "pdo";
+
+- Criar um tabela com o nome "login" e consiste em três colunas:
+  . id (como Primary Key e Auto Inserida, isso faz com que o id sempre seja diferente para cada cadastro);
+  . nome;
+  . login;
--- conexao.php
+++ conexao.php
@@ -6,13 +6,3 @@ try {
 } catch (PDOException $e) {
     echo "Falha ao conectar com o banco de dados" . $e->getMessage();
 }
-
-
-    //   entra no localhost/phpmyadmin/ e cria um novo banco de dados com o nome pdo
-    //   nesse banco criamos uma tabela com o nome 'login' que possui
-    //   id
-    //   nome 
-    //   login
-
-    // faz conexão com o banco de dados
-    // localhost/pdo/conexao.php para verificar se está conectando ao banco
